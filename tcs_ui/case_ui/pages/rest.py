4d548cc359c723983489f92f7a33a1fd270df54f
beta-things <paul@beta-things.com>
2023-05-18 10:41:08 -0700

    working update?

--- tcs_ui/case_ui/pages/rest.py
+++ tcs_ui/case_ui/pages/rest.py
@@ -24,6 +24,7 @@ def check(request):
 
 def cust_ip(request):
 	if request.method == 'GET':
+		testvar = ""
 		pipe_path = './compipe/compipe'
 		pipe_fd = os.open(pipe_path, os.O_WRONLY)
 		print('pipe connected')
