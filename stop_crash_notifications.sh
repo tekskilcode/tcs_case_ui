c928abda6456a63a9e8b45a7343449aca010bb1b
beta-things <53658721+beta-things@users.noreply.github.com>
2023-08-07 15:47:07 -0700

    Update stop_crash_notifications.sh add SSH SUPPORT AND NET TOOLS

--- stop_crash_notifications.sh
+++ stop_crash_notifications.sh
@@ -3,3 +3,5 @@
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
 sudo systemctl stop apport.service
 sudo apt-get remove update-notifier
+sudo apt-get install ssh
+sudo apt-get install net-tools
