dbba760ce95fe80a9bfd028f2cc6e6e22435818f
beta-things <paul@beta-things.com>
2023-09-07 17:27:37 -0700

    update usb scripts

--- install/usb_scripts/tcs_ui_prereq.sh
+++ install/usb_scripts/tcs_ui_prereq.sh
@@ -18,10 +18,20 @@ sudo -u $SUDO_USER git clone https://github.com/beta-things/tcs_case_ui.git /usr
 
 sudo ln -s /usr/local/opt/apps /home/tch/apps
 
+echo "///Setup Docker///"
+sudo sh /home/tch/apps/tcs_ui/install/install_docker.sh
+
+echo "///Install WEB SERVER ///"
+#sudo SUDO_USER="$SUDO_USER" sh  ./install_web_server.sh
+#extract contents to Downloads
+tar -xf tkskl-server-setup.tar -C /home/tch/Downloads
+
+cd /home/tch/Downloads/tkskl-server-setup
+
+sudo SUDO_USER=$SUDO_USER ./setup_server.sh
+
 cd /home/tch/apps/tcs_ui/install
 
-echo "///Setup Docker///"
-sudo ./install_docker.sh
 
 echo "///Install Chromium and autostart browser///"
 sudo SUDO_USER="$SUDO_USER" sh  ./install_chromium_services.sh
@@ -35,6 +45,7 @@ sudo ./stop_crash_notifications.sh
 
 echo "///set interface name///"
 sudo ./change_net_interface.sh
+
 
 echo "///Install DisplayLink driver///"
 #sudo ./DisplayLink_USB_Ubuntu_5.6.1/displaylink-driver-5.6.1-59.184.run
