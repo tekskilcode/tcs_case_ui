96f433b7262ed2ca656d958671683ba4cc52147e
Matt Johnson <matt.johnson.dev@gmail.com>
2024-02-25 14:30:03 -0700

    adjustments to install_services

--- install/install_services.sh
+++ install/install_services.sh
@@ -2,11 +2,11 @@
 
 #sudo systemctl daemon-reload
 
-sudo cp service_files/tcs_case_ui.service /etc/systemd/system/
-sudo cp service_files/tcs_pipe.service /etc/systemd/system/
-sudo cp service_files/tcs_web_ui.service /etc/systemd/system/
-sudo cp service_files/x11vnc@gdm.service /etc/systemd/system/
-sudo cp service_files/x11vnc@tch.service /etc/systemd/system/
+sudo cp -rf service_files/tcs_case_ui.service /etc/systemd/system/
+sudo cp -rf service_files/tcs_pipe.service /etc/systemd/system/
+sudo cp -rf service_files/tcs_web_ui.service /etc/systemd/system/
+sudo cp -rf service_files/x11vnc@gdm.service /etc/systemd/system/
+sudo cp -rf service_files/x11vnc@tch.service /etc/systemd/system/
 
 sudo systemctl enable tcs_case_ui.service 
 sudo systemctl enable tcs_pipe.service
@@ -27,3 +27,4 @@ sudo mkfifo /home/tch/apps/tcs_ui/tcs_ui_pipe/compipe
 sudo apt install -y gnome-shell-extensions
 
 sudo -u tch gnome-extensions install disable-gestures-2021verycrazydog.gmail.com.v4.shell-extension.zip
+
