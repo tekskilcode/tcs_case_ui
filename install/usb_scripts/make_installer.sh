5159acaa49324752c347736bb76ac9b761a019d5
Matt Johnson <matt.johnson.dev@gmail.com>
2024-02-27 12:04:43 -0700

    USB installer generator

--- install/usb_scripts/make_installer.sh
+++ install/usb_scripts/make_installer.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
-# This expects the install.sh file in bootstrap/, which is not included in the repo (it contains many secret environment variables)
+# This generates the install.sh file in the directory ./bootstrap
+# The environment variables must be set becore calling this script.
+# You may be asked for a password in order to install "makeself"
+
+# GITHUB_USERNAME
+# GITHUB_TOKEN
+# VNC_PASSWD
+# TAILSCALE_AUTHKEY
+
+rm tkskl_setup.run
+rm -f bootstrap/install.sh
+
+tee ./bootstrap/install.sh <<EOT
+#!/bin/bash
+
+export GITHUB_USERNAME=$GITHUB_USERNAME
+export GITHUB_TOKEN=$GITHUB_TOKEN
+export VNC_PASSWD='$VNC_PASSWD'
+export TAILSCALE_AUTHKEY=$TAILSCALE_AUTHKEY
+
+if [[ $UID = 0 ]]; then
+    echo "Do not run this script as root."
+    exit 1
+fi
+
+sudo apt update
+sudo apt install -y curl
+
+curl -s https://raw.githubusercontent.com/tekskilcode/tcs_case_ui/master/install/usb_scripts/tcs_ui_prereq.sh | bash
+EOT
 
 chmod +x bootstrap/install.sh
 sudo apt install makeself
