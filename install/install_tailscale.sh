9b6ae8f027df2877b6790a6d4ebec38f65d0b617
beta-things <53658721+beta-things@users.noreply.github.com>
2024-04-08 11:06:06 -0700

    Update install_tailscale.sh
    kyle req update ssh native only

--- install/install_tailscale.sh
+++ install/install_tailscale.sh
@@ -5,7 +5,7 @@ curl -fsSL https://tailscale.com/install.sh | sh
 
 # https://tailscale.com/kb/1193/tailscale-ssh/
 #sudo tailscale up --ssh
-sudo tailscale up --ssh --accept-risk=lose-ssh --authkey $TAILSCALE_AUTHKEY
+sudo tailscale up --authkey $TAILSCALE_AUTHKEY
 
 sudo systemctl enable --now tailscaled
 sudo systemctl start tailscaled
